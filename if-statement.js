a9faf7d654d7afa99965c88678eb0bcf94073352
Alex Tilatti <alex.tilatti@gmail.com>
2019-02-05 18:20:30 +0000

    Initial commit with all my work so far

---
+++ if-statement.js
@@ -0,0 +1,9 @@
+var fruit = 'orange';
+var vegetable = fruit.toString();
+
+if (vegetable.length > 5) {
+  console.log('The fruit name has more than five characters.');
+}
+else {
+  console.log('The fruit name has five characters or less.');
+}
